87e5cf20ae7c44c11cafe93307394d00145d4a06
Lucas Alves <phytolinedev@gmail.com>
2022-02-28 02:01:09 -0300

    add content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
+    document.documentElement.classList.add('dark')
 } else {
-  document.documentElement.classList.remove('dark')
+    document.documentElement.classList.remove('dark')
+}
+
+const switchMode = (event) => {
+    switch (event.value) {
+        case "auto":
+            localStorage.removeItem('theme')
+            break;
+        case "light":
+            localStorage.theme = 'light'
+            break;
+        case "dark":
+            localStorage.theme = 'dark'
+            break;
+        default:
+            break;
+    }
+};
+
+function toggleMenu(flag) {
+    let value = document.getElementById("menu");
+    if (flag) {
+        value.classList.remove("hidden");
+    } else {
+        value.classList.add("hidden");
+    }
 }
