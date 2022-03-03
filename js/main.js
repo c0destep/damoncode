ff1c0aaeed2ee3c5b1ae24a8579b87046dd75952
Lucas Alves <phytolinedev@gmail.com>
2022-03-02 23:13:17 -0300

    add form contact

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
 window.onload = () => {
     verifyTheme();
+    backToTop();
+
+    let formSendContact = document.getElementById('formSendContact');
+    let buttonSendContact = document.getElementById('buttonSendContact');
+    let hasSend = document.getElementById('hasSend');
+    let hasNotSend = document.getElementById('hasNotSend');
+
+    buttonSendContact.addEventListener('click', async () => {
+        buttonSendContact.disable = true;
+
+        let Data = new FormData(formSendContact);
+
+        let response = await fetch(formSendContact.action, {
+            method: formSendContact.method,
+            body: Data
+        });
+
+        if (response.status === 200) {
+            let result = await response.json();
+
+            buttonSendContact.disable = false;
+
+            if (result.status === 'error') {
+                hasNotSend.classList.toggle('hidden', false);
+                hasNotSend.innerText = result.message;
+
+                setTimeout(function () {
+                    hasNotSend.classList.toggle('hidden', true);
+                }, 5000);
+            } else {
+                hasSend.classList.toggle('hidden', false);
+                hasSend.innerText = result.message;
+
+                setTimeout(function () {
+                    hasSend.classList.toggle('hidden', true);
+                    formSendContact.reset();
+                }, 5000);
+            }
+        } else {
+            hasNotSend.classList.toggle('hidden', false);
+            hasNotSend.innerText = 'Lamentamos que houver um erro no envio da mensagem!!';
+
+            setTimeout(function () {
+                hasNotSend.classList.toggle('hidden', true);
+            }, 5000);
+        }
+    });
 }
 
 window.onscroll = () => {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("backToTop").classList.toggle('inline-flex', true);
-        document.getElementById("backToTop").classList.toggle('hidden', false);
-    } else {
-        document.getElementById("backToTop").classList.toggle('inline-flex', false);
-        document.getElementById("backToTop").classList.toggle('hidden', true);
-    }
+    backToTop();
 }
 
 const switchMode = (event) => {
@@ -54,5 +95,15 @@ function verifyTheme() {
         document.documentElement.classList.add('dark');
     } else {
         document.documentElement.classList.remove('dark');
+    }
+}
+
+function backToTop() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById("backToTop").classList.toggle('inline-flex', true);
+        document.getElementById("backToTop").classList.toggle('hidden', false);
+    } else {
+        document.getElementById("backToTop").classList.toggle('inline-flex', false);
+        document.getElementById("backToTop").classList.toggle('hidden', true);
     }
 }
